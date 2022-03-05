124305fdde6f8c6f7204a5225dcdc2f374827523
Sergei Malevannyi <bbserg@yandex.ru>
2022-03-05 15:29:48 +0200

    Solution for Task 1.2

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -2,8 +2,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass {
+
     @Test
     public void testGetLocalNumber() {
-        Assert.assertTrue("Error: getLocalNumber() doesn't return 14", this.getLocalNumber() == 14);
+        Assert.assertTrue("Error: getLocalNumber() returns " + this.getLocalNumber() + " which is not 14" , this.getLocalNumber() == 14);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("Error: getLocalNumber() returns " + this.getLocalNumber() + " which less than 45", this.getClassNumber() > 45);
     }
 }
